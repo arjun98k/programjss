b7f2f768a85181d576037ac555adbbf594b82737
arjun98k <arjunkandekar14@gmail.com>
2024-03-10 01:09:16 +0530

    update to arrays

--- Arraays.js
+++ Arraays.js
@@ -1,6 +1,11 @@
-var a = [12,55,66,34]
+var a = ['apple','orange','mangoes']
 console.log(a);
 
-a[0] = 11
+// a[0] = 11
 
 console.log(a);
+
+console.log(a.length);
+let b = a.toString()
+console.log(b)
+
