8e0936f0fcddfc1bf3eba744879cf5e86d614046
arjun98k <arjunkandekar14@gmail.com>
2024-03-10 23:54:15 +0530

    update to arrays

---
+++ LoopsArray.js
@@ -0,0 +1,11 @@
+arr = [11,12,13,14,15]
+newarr = []
+
+
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    newarr.push(element*2)
+    console.log(index)
+    
+}
+console.log(newarr)
