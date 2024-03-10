b8e16b6b683e54ff664a165d537ff1c1e85638cf
arjun98k <arjunkandekar14@gmail.com>
2024-03-10 18:31:04 +0530

    update to arrays

--- Array_2.js
+++ Array_2.js
@@ -1,3 +1,10 @@
 let array = [11,12,13,16,165]
 
-console.log(array.join("-"))
+// console.log(array.join("-"))
+
+console.log(array.pop())
+console.log(array)
+console.log(array.push('arjun'))
+console.log(array)
+array.push(98)
+console.log(array)
