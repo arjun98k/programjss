f9038c86296f8e0a95f3c4427b4f1a62030d4963
arjun98k <arjunkandekar14@gmail.com>
2024-03-10 18:44:24 +0530

    update to arrays

--- Array_2.js
+++ Array_2.js
@@ -15,3 +15,12 @@ console.log(array)        //or hai push ka bhai jo element start mai add karta h
 
 delete array[2]
 console.log(array)      // delete the value but not the memory
+
+let array1 = [11,111,12,14]
+let array2 = [154,16,17,201,199,400]
+let array3 = [18,19,20]
+
+var array5 = array1.concat(array2,array3)
+console.log(array5)
+array5.sort()
+console.log(array5)
