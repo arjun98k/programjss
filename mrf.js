c9d5add28c06edc256b9aeb0047237a70536f554
arjun98k <arjunkandekar14@gmail.com>
2024-03-12 00:15:33 +0530

    update to arrays

--- mrf.js
+++ mrf.js
@@ -1,6 +1,22 @@
-let arr = [12,131,14,15,16,17]
+// let arr = [12,131,14,15,16,17]
 
-let newarr = arr.map(e => {
-    return e*3
-})
-console.log(newarr)
+// let newarr = arr.map(e => {
+//     return e*3
+// })
+// console.log(newarr)
+
+
+// var greatthanseven = (e =>{
+//     if (e>13) {
+//         return true
+//     }
+//     return false
+// })
+// console.log(arr.filter(greatthanseven))
+
+
+let aar = [11,13,14]
+const red = (a,b)=> {
+    return a+b
+}
+console.log(aar.reduce(red))
