28eb3a667b5d11615a2f5ed7e68651984c883a24
arjun98k <arjunkandekar14@gmail.com>
2024-03-16 23:39:57 +0530

    update to arrays

--- Dom manpulation/Dom manipulator/Dom2/akk.js
+++ Dom manpulation/Dom manipulator/Dom2/akk.js
@@ -10,3 +10,5 @@ document.getElementById('pink2').style.backgroundColor = "yellow"
 
 document.getElementById("pink3").style.color = "pink";
 document.getElementById('pink3').style.backgroundColor = "green"
+
+console.log("hello")
