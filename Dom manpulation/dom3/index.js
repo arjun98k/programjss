a3d85a58de3b527437c2ca68d061300936e7dc3b
arjun98k <arjunkandekar14@gmail.com>
2024-03-20 00:21:19 +0530

    update to arrays

--- Dom manpulation/dom3/index.js
+++ Dom manpulation/dom3/index.js
@@ -13,3 +13,7 @@ for (let index = 0; index < 17; index++) {
     console.log("wpl champion rcb and mumbai indian")
     
 }
+for (let index = 0; index < 25; index++) {
+    console.log("hello laundda")
+    
+}
